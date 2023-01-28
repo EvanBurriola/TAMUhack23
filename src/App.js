abd7c15803ff017159d5ea89e72eb02f17bbc65d
Evan Burriola <evanburriola12@tamu.edu>
2023-01-28 16:23:50 -0600

    Basic to do list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 import './App.css';
 
 function App() {
+  const [todos, setTodos]= useState([]);
+
+  function addTodo(todo){
+    setTodos([todo, ...todos]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>To do list</p>
+        <TodoForm addTodo={addTodo}/>
+        <TodoList todos={todos}/>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
